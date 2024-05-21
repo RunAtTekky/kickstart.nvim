c9be85c09f3fc4d267be487b8b9e9f8d5aedc4e2
Varun <varunrawat343@gmail.com>
2024-05-21 19:38:26 +0530

    Read learnxinyminutes lua

--- init.lua
+++ init.lua
@@ -1,9 +1,4 @@
 --[[
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
     After understanding a bit more about Lua, you can use `:help lua-guide` as a
     reference for how Neovim integrates Lua.
     - :help lua-guide
